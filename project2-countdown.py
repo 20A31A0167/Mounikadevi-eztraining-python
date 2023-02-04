00b5b56800f92c57186666cfe9770a7614345fec
20A31A0167 <124118456+20A31A0167@users.noreply.github.com>
2023-02-04 15:37:43 +0530

    Add files via upload

---
+++ project2-countdown.py
@@ -0,0 +1,14 @@
+import time
+def countdown(t):
+
+
+    while t:
+        mins, secs = divmod(t,60)
+        timer = '{:02d}:{:02d}'.format(mins,secs)
+        print(timer)
+        time.sleep(10)
+        t -=1
+
+    print('FINISHED')
+t=input("enter the time in seconds: ")
+countdown(int(t))
